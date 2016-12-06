61ddd2396dca50bdb68029fb69a69415c7d3a1bb
DennysR <dennysrojas@hotmail.es>
2016-12-05 22:43:02 -0600

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Funcionario:Pesona
+    {
+        private String listaPaciente;
+        public String ListaPaciente
+        {
+            get
+            {
+                return this.listaPaciente;
+            }
+
+            set
+            {
+                this.listaPaciente = " ";
+            }
+        }
+        private String listaMedico;
+        public String ListaMedico
+        {
+            get
+            {
+                return this.listaMedico;
+            }
+
+            set
+            {
+                this.listaMedico = " ";
+            }
+        }
+    }
+}
