591d06bf975f52687219eed7d7dc7936577ed8d2
MichelleJaramillo <m.nata1797@hotmail.com>
2016-12-05 23:46:50 -0500

    Add files via upload

---
+++ Direccion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Direccion
+    {
+        public String callePrincipal;
+        public String CallePrincipal
+        {
+            get
+            {
+                return this.callePrincipal;
+            }
+
+            set
+            {
+                this.callePrincipal = " ";
+            }
+        }
+        public String calleSecundaria;
+        public String CalleSecundaria
+        {
+            get
+            {
+                return this.calleSecundaria;
+            }
+
+            set
+            {
+                this.calleSecundaria = " ";
+            }
+        }
+        public String numeracion;
+        public String Numeracion
+        {
+            get
+            {
+                return this.numeracion;
+            }
+
+            set
+            {
+                this.numeracion = " ";
+            }
+        }
+    }
+}
