513762c9b070add457e65e250524890411f86412
jessicaaltamirano <jessy2995@hotmail.com>
2016-12-05 23:52:12 -0500

    Add files via upload

---
+++ Seguro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Seguro
+    {
+        public String numeroSeguro;
+        public String NumeroSeguro
+        {
+            get
+            {
+                return this.numeroSeguro;
+            }
+
+            set
+            {
+                this.numeroSeguro = " ";
+            }
+        }
+
+    }
+}
